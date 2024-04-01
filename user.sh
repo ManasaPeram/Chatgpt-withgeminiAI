9421ce0f95ae12d9233efbc43a03729a365e11bc
manasaperam <manasaperam1980@gmail.com>
2024-04-01 16:29:30 +0530

    gpt-geminiai-pro-1.0

--- user.sh
+++
@@ -1,3 +0,0 @@
-git config --global user.email "manasaperam1980@gmail.com"
-git config --global user.name "manasaperam"
-git remote add origin https://github.com/ManasaPeram/reactnative-googleauthentication.git
